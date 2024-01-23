4723e1db1bab35bd9a3d28642a012914829257a9
mehmeterayserter <mehmeterayserter@gmail.com>
2024-01-23 15:38:26 +0300

    database configurations changed.

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -6,9 +6,7 @@ export async function POST(req: Request) {
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local",
-    );
+    throw new Error("Please add WEBHOOK_SECRET from Clerk Dashboard to .env");
   }
 
   const headerPayload = headers();
