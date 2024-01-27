67df540799c5818eaddb94e8127338ffe075ccd7
Mehmet Eray Serter <mehmeterayserter@gmail.com>
2024-01-27 17:22:04 +0300

    example endpoint added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,20 @@
+"use client";
+
+import React, {useEffect} from "react";
+
 export default function Home() {
+  const [data, setData] = React.useState(null)
+
+  useEffect(() => {
+    fetch('api/stocks/news')
+        .then(res => res.json())
+        .then(data => setData(data))
+  })
+
+  if (!data) return <div>Loading...</div>
+
+  console.log(data)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
